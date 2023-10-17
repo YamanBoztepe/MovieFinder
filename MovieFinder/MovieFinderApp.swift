15186f5b9082bc975418082fdf9349ec6363a8fa
Yaman Boztepe <yaman.boztepe@appcent.mobi>
2023-10-17 16:50:07 +0300

    Initial Commit

---
+++ MovieFinder/MovieFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFinderApp.swift
+//  MovieFinder
+//
+//  Created by Yaman Boztepe on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
